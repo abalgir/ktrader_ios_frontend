a4812e14d0e23fd8dab59ba216e2fcb252427a4d
abalgir <abalgir@gmaIL.com>
2026-03-18 15:56:43 -0500

    Initial Commit

---
+++ KTrader/KTraderApp.swift
@@ -0,0 +1,17 @@
+//
+//  KTraderApp.swift
+//  KTrader
+//
+//  Created by Gerard Rafie on 3/18/26.
+//
+
+import SwiftUI
+
+@main
+struct KTraderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
